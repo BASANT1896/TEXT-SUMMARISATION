2f95bd4f07f39cd274dd1571ca8f30302e43908f
BASANT1896 <ranimahaur1984@gmail.com>
2026-06-26 20:41:54 +0530

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Replace the ID with your Google Drive file ID
-FILE_ID="YOUR_GOOGLE_DRIVE_FILE_ID"
-URL="https://drive.google.com/uc?export=download&id=$FILE_ID"
-
-echo "Downloading model from Google Drive..."
-curl -L "$URL" -o model.zip
-unzip model.zip -d ./model_folder
-rm model.zip
-echo "Model ready."
+# This downloads your model folder from Drive and names the local folder 'saved_summary_model'
+gdown --folder "https://drive.google.com/drive/folders/1Mwbq3zXEyxff5uePsMEvAYOGXEMnm8Ry?usp=sharing" -O ./saved_summary_model
